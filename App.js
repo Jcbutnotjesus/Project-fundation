f3ff9e4232c8e568140549e6c7b93dc641d2f68c
cornelis <jancornelis.pro@gmail.com>
2022-02-01 15:25:27 +0100

    push

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const queryClient = new QueryClient()
 
 function App() {
   return (<QueryClientProvider client={queryClient}>
-    <FeedScreen></FeedScreen>
+    <FeedScreen/>
 </QueryClientProvider>)
 }
 
